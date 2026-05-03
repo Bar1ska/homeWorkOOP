79c4dc65e63f895dd20afd61e9276612a80feae9
Николай Широлапов <nikolaishirolapov@yandex.ru>
2026-05-03 22:44:59 +0300

    дз 6.2

--- homeWorkOOP.cpp
+++ homeWorkOOP.cpp
@@ -4,100 +4,70 @@
 #include <vector>
 #include <conio.h>
 #include <fstream>
+#include <stdio.h>
 using namespace std;
 
-class PETS
+class Line
 {
 public:
-    void input()
+    std::vector <char> str;
+    void getStr()
     {
-        system("CLS");
-        cout << "Введите имя питомца: ";
-        cin >> name;
-        namePet.push_back(name);
-        cout << "Введите характеристику питомца: ";
-        cin >> characteristic;
-        characteristicPet.push_back(characteristic);
-    }
-    void output() 
-    {
-        system("CLS");
-        for (int i = 0; i < namePet.size(); ++i) 
+        while ((symbol = getchar()) != '\n')
         {
-            cout << namePet[i] << " — " << characteristicPet[i] << endl;
+            str.push_back(symbol);
         }
-        system("pause");
     }
-
+    int giveSize()
+    {
+        return str.size();
+    }
+    void deleteLine()
+    {
+        str.clear();
+    }
+    void adding(std::vector <char> str2)
+    {
+        for (int i = 0; i < str2.size(); ++i)
+        {
+            str.push_back(str2[i]);
+        }
+    }
+    bool checkEquality(std::vector<char> str2) 
+    {
+        if (giveSize() != str2.size()) 
+        {
+            return false;
+        }
+        for (int i = 0; i < giveSize(); ++i) 
+        {
+            if (str[i] != str2[i]) 
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    Line() = default;
+    Line(const std::vector <char> str2) 
+    {
+        str = str2;
+    }
+    Line(const Line& obj)
+    {
+        str = obj.str;
+    }
+    ~Line() 
+    {
+        deleteLine();
+    }
 private:
-    string name, characteristic;
-    std::vector<string> namePet;
-    std::vector<string> characteristicPet;
+    char symbol;
 };
 
-class DOG : public PETS {};
-class CAT : public PETS {};
-class PARAKEET : public PETS {};
 int main()
 {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    DOG dog;
-    CAT cat;
-    PARAKEET parakeet;
-    int choice = 0, choicePet;
-    while (choice != 3) 
-    {
-        system("CLS");
-        cout << "Выберите:" << endl;
-        cout << "1. Ввод имени и харектристики питомца" << endl;
-        cout << "2. Вывод имен и характеристик питомца" << endl;
-        cout << "3. Выход из прогаммы" << endl;
-        cout << "Вариант -> ";
-        cin >> choice;
-        switch (choice) 
-        {
-        case 1:
-            cout << "Выберите животное:" << endl;
-            cout << "1. батердог" << endl;
-            cout << "2. котость" << endl;
-            cout << "3. попуг" << endl;
-            cout << "Питомец -> ";
-            cin >> choicePet;
-            switch (choicePet)
-            {
-            case 1:
-                dog.input();
-                break;
-            case 2:
-                cat.input();
-                break;
-            case 3:
-                parakeet.input();
-                break;
-            }
-            break;
-        case 2:
-            cout << "Выберите животное:" << endl;
-            cout << "1. батердог" << endl;
-            cout << "2. котость" << endl;
-            cout << "3. попуг" << endl;
-            cout << "Питомец -> ";
-            cin >> choicePet;
-            switch (choicePet)
-            {
-            case 1:
-                dog.output();
-                break;
-            case 2:
-                cat.output();
-                break;
-            case 3:
-                parakeet.output();
-                break;
-            }
-            break;
-        }
-    }
 }
 
